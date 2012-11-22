2f997accd2d4e104e980b44cf152e5eb79e6c526
Rathi <rathibose.p@gmail.com>
2012-11-22 13:51:57 +0800

    With Digit and Zero Restrictions

---
+++ Bazz.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+<title>numberdisplay</title>
+<script>
+function callfunctions()
+{
+var b=positiveInt();
+if(b) loadXMLDoc();
+return b;
+}
+function positiveInt()
+{
+//To check if input is valid positive integer
+var fromval=document.getElementById("txtfrom").value;
+var toval=document.getElementById("txtto").value;
+//To check if valid From integer
+if(isNaN(fromval) || fromval <= 0) 
+{
+ 	  alert("Please enter a valid \"From\" positive integer");
+	  return false;
+}
+//To check if valid To integer
+if(isNaN(toval) || toval <= 0) 
+{
+	alert("Please enter a valid \"To\" positive integer");
+	return false;
+}
+//To check if valid range
+if(parseInt(fromval,10)>=parseInt(toval,10))
+{
+	alert("Please enter valid ascending range");
+	return false;
+}
+else
+return true;
+}
+
+function loadXMLDoc()
+{
+//AJAX
+var from=document.getElementById("txtfrom").value;
+var to=document.getElementById("txtto").value;
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("resp").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","ajax_bazz.php?from="+from+"&to="+to,true);
+xmlhttp.send();
+}  
+</script>
+</head><body>
+<form name="numdisplay" id="numdisplay" method="get">
+  <table style="text-align: center; width: 604px; height: 149px;" border="1" cellpadding="2" cellspacing="2" align="center">
+    <tbody>
+      <tr>
+        <td colspan="2" rowspan="1" style="vertical-align: top; width: 292px;">Enter range of numbers to
+display<br>
+        </td>
+      </tr>
+      <tr>
+        <td style="vertical-align: top; width: 292px;">From : <input name="txtfrom" id="txtfrom" onkeyup="this.value=this.value.replace(/\D/,'')"></td>
+        <td style="vertical-align: top; width: 292px;">To : <input name="txtto" id="txtto" onkeyup="this.value=this.value.replace(/\D/,'')"><br>
+        </td>
+      </tr>
+      <tr align="center">
+        <td colspan="2" rowspan="1" style="vertical-align: top; width: 292px;"><input name="submit" value="Display" onclick="callfunctions()" type="button"><br>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2" rowspan="1" style="vertical-align: top; width: 292px;" id="resp"><br>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <br>
+</form>
+</body>
+</html>
---
+++ ajax_bazz.php
@@ -0,0 +1,32 @@
+<?php
+//Code to Build Ajax Response to print Bazz for integer following consecutive Fizz/Buzz
+
+$from=$_GET["from"];
+$to=$_GET["to"];
+for($i=$from;$i<=$to;$i++)
+{
+if((($i%3)==0)&(($i%5)==0))
+	$range[]="FizzBuzz";
+if(($i%3)==0)
+	$range[]="Fizz";
+else if(($i%5)==0)
+	$range[]="Buzz";
+else
+	$range[]=$i." ";
+}
+
+for($j=0;$j<($to-$from);$j++)
+{
+if((($range[$j]=="Fizz")&&($range[$j+1]=="Buzz"))||(($range[$j]=="Buzz")&&($range[$j+1]=="Fizz"))||(($range[$j]=="FizzBuzz")&&($range[$j+1]=="Fizz"))||(($range[$j]=="FizzBuzz")&&($range[$j+1]=="Buzz")))
+{
+	if(($j+2)<($to-$from))
+	$range[$j+2]="Bazz";
+}
+}
+$response=$range;
+
+header("Content-type: application/json");
+    echo json_encode($range);
+
+
+?>
