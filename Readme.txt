c2736c8f0f0c6e255c014b510f3515972982feee
Rathi <rathibose.p@gmail.com>
2012-11-23 00:30:08 +0800

    FizzBuzz Before Validation

--- Readme.txt
+++ Readme.txt
@@ -1,27 +1,27 @@
 --------------------------------------------------------------------------------------------
 Project	:	FizzBuzz
-Version	:	1.1
+Version	:	1.0
 Author	:	Pandi Rathi
-Date	:	21-Nov-2012
+Date	:	20-Nov-2012
 --------------------------------------------------------------------------------------------
-Installation Notes:
-------------------
+Pre-Requisites:
+---------------
 
 1. Install web server (LAMP/WAMP)
 2. Copy fizzbuzz.php and ajax.php to the document root
-3. Use browser to view fizzbuzz.php
 
-Features common to version 1.0
---------------------------------
+
+Features:
+---------
 1. Gets input range from the user
 2. Displays all numbers between the specified range but with "Fizz" for multiples of 3 and "Buzz" for multiples of 5
 
-New Features in Version 1.1
------------------------------
+Known Issues:
+--------------
 1. Validations for the input range
-	a. Input are integers and not any other characters
-	b. Integers are positive
-	c. Range is ascending ( Range end > Range Begin)
-	
-2. Displays "FizzBuzz" replacing numbers which are both multiples of 3 and 5
 
+Assumptions:
+-------------
+
+1. Webserver and Mysql are running with no issues
+2. Browser is made to view fizzbuzz.php in the document root
--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,57 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
 <html><head>
-  
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-  <title>numberdisplay</title>
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
 
-  
-  
-  <style type="text/css">
-.line1 {
-  background-color: silver;
-}
-.line2 {
-  border-style: dotted;
-  background-color: #e9e9e9;
-}
+<!--Form For getting input and Display Results-->
 
-  </style>
-  
-  <script>
-
-function callfunctions()
-{
-var b=positiveInt();
-if(b) loadXMLDoc();
-return b;
-}
-
-function positiveInt()
-{
-//To check if input is valid positive integer
-var fromval=document.getElementById("txtfrom").value;
-var toval=document.getElementById("txtto").value;
-//To check if valid From integer
-if(isNaN(fromval) || fromval <= 0) 
-{
- 	  alert("Please enter a valid \"From\" positive integer");
-	  return false;
-}
-//To check if valid To integer
-if(isNaN(toval) || toval <= 0) 
-{
-	alert("Please enter a valid \"To\" positive integer");
-	return false;
-}
-//To check if valid range
-if(fromval>=toval)
-{
-	alert("Please enter valid ascending range");
-	return false;
-}
-else
-return true;
-}
+<title>numberdisplay</title>
+<script>
 function loadXMLDoc()
 {
 //AJAX
@@ -76,23 +31,23 @@ xmlhttp.onreadystatechange=function()
 xmlhttp.open("GET","ajax.php?from="+from+"&to="+to,true);
 xmlhttp.send();
 }  
-  </script>
-</head><body>
+</script>
+</head>
+<body>
 <form name="numdisplay" id="numdisplay" method="get">
-  <table style="text-align: left; width: 604px; height: 149px;" border="1" cellpadding="2" cellspacing="2">
-    <tbody>
+<table style="text-align: left; width: 604px; height: 149px;" border="1" cellpadding="2" cellspacing="2">
+ <tbody>
       <tr>
         <td colspan="2" rowspan="1" style="vertical-align: top; width: 292px;">Enter range of numbers to
 display<br>
         </td>
       </tr>
       <tr>
-        <td style="vertical-align: top; width: 292px;">From : <input name="txtfrom" id="txtfrom" onkeyup="this.value=this.value.replace(/\D/,'')"></td>
-        <td style="vertical-align: top; width: 292px;">To : <input name="txtto" id="txtto" onkeyup="this.value=this.value.replace(/\D/,'')"><br>
-        </td>
+        <td style="vertical-align: top; width: 292px;">From : <input name="txtfrom" id="txtfrom"></td>
+        <td style="vertical-align: top; width: 292px;">To : <input name="txtto" id="txtto"><br></td>
       </tr>
       <tr align="center">
-        <td colspan="2" rowspan="1" style="vertical-align: top; width: 292px;"><input name="submit" value="Display" onclick="callfunctions()" type="button"><br>
+        <td colspan="2" rowspan="1" style="vertical-align: top; width: 292px;"><input name="submit" value="Display" onclick="loadXMLDoc()" type="button"><br>
         </td>
       </tr>
       <tr>
@@ -104,5 +59,4 @@ display<br>
   <br>
   <br>
 </form>
-
 </body></html>
